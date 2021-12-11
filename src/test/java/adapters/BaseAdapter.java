18c4572a0185d9212b884d648acc6438b20c8476
evgeniygapontsev <gapontsev91@gmail.com>
2021-12-11 23:41:37 +0300

    Added Api tests

--- src/test/java/adapters/BaseAdapter.java
+++ src/test/java/adapters/BaseAdapter.java
@@ -7,9 +7,10 @@ import static io.restassured.RestAssured.given;
 
 @Log4j2
 public class BaseAdapter {
-    final String BASE_URL_API = System.getenv().getOrDefault("TESTRAIL_URL_API", PropertyReader.getProperty("testrail.url.api"));
-    final String TOKEN = System.getenv().getOrDefault("TESTRAIL_API_TOKEN", PropertyReader.getProperty("testrail.api.token"));
-
+//    final String BASE_URL_API = System.getenv().getOrDefault("TESTRAIL_URL_API", PropertyReader.getProperty("testrail.url.api"));
+//    final String TOKEN = System.getenv().getOrDefault("TESTRAIL_API_TOKEN", PropertyReader.getProperty("testrail.api.token"));
+    final String BASE_URL_API = "https://gapontsevevg.testrail.io/index.php?/api/v2/";
+    final String TOKEN = "Basic Z2Fwb250c2V2X3Rlc3RyYWlsQG1haWxpbmF0b3IuY29tOndkTk1PM1NnVTZHU0o2WEtrQi8y";
     public String post(String body, String uri) {
         given().
                 header("Content-Type", "application/json").
